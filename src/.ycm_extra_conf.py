01a12a261191a79fca7ace92fb27a64f8e1a56d6
FooFooDamon <udc577@126.com>
2023-11-08 16:05:38 +0800

    src/.ycm_extra_conf.py: Support app source code parsing.

--- src/.ycm_extra_conf.py
+++ src/.ycm_extra_conf.py
@@ -3,6 +3,22 @@
 import os, sys
 YCM_CONF_DIR = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(YCM_CONF_DIR, "..", "3rdpary", "lazy_coding", "python"))
-from ycm_conf_for_linux_driver import *
-flags.extend([ "-I", YCM_CONF_DIR, '-DKBUILD_MODNAME="dual_pcan_usb"' ])
+from ycm_conf_for_c_and_cpp import flags as app_flags
+import ycm_conf_for_linux_driver
+
+APP_SRC_BASENAMES = [
+    "setting_app",
+]
+
+def FlagsForFile(filename: str, **kwargs):
+#{#
+    base_name = os.path.splitext(os.path.basename(filename))[0]
+    driver_flags = ycm_conf_for_linux_driver.flags
+
+    return { "flags": app_flags if base_name in APP_SRC_BASENAMES else driver_flags }
+#}#
+
+ycm_conf_for_linux_driver.FlagsForFile = FlagsForFile
+
+Settings = ycm_conf_for_linux_driver.Settings
 
