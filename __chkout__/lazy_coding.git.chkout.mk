fa27265c1e766f71f8c039399370a6042ca0652a
FooFooDamon <udc577@126.com>
2023-12-18 23:04:20 +0800

    __chkout__/lazy_coding.git.chkout.mk: (1) Update the version of c_and_cpp/native/chardev_group.{c,h} to 73a04dd9a2a6; (2) Add c_and_cpp/native/devclass_supplements.{c,h}.

--- __chkout__/lazy_coding.git.chkout.mk
+++ __chkout__/lazy_coding.git.chkout.mk
@@ -13,8 +13,10 @@ export CHKOUT_PARTIAL_ITEMS := main/c_and_cpp/native/__ver__.h \
     961a8ad599580425a6e0dacd05cc2bccb0de49ac/makefile/linux_driver.mk \
     217ebcbe23a78dacf7491685ef2e121a0721eb62/c_and_cpp/native/klogging.h \
     cf8d6071d6efcf148ddaa069ec0b347b1fd0192e/c_and_cpp/native/evol_kernel.h \
-    a6e5317c9b43f2aa343216e38a0b6e0f7d623251/c_and_cpp/native/chardev_group.c \
-    a6e5317c9b43f2aa343216e38a0b6e0f7d623251/c_and_cpp/native/chardev_group.h \
+    73a04dd9a2a6f44bc4100951eeebcdbb91f4437c/c_and_cpp/native/chardev_group.c \
+    73a04dd9a2a6f44bc4100951eeebcdbb91f4437c/c_and_cpp/native/chardev_group.h \
+    9077ab1f647f017bb982eb0bacf08698386e8f84/c_and_cpp/native/devclass_supplements.c \
+    9077ab1f647f017bb982eb0bacf08698386e8f84/c_and_cpp/native/devclass_supplements.h \
     0ec0fc48ae88c0a69f8378691c30ba5ad0d19a7b/python/ycm_conf_for_c_and_cpp.py \
     a85cce2c2cc8ac1725a3cad7af09c71f8a6b5a89/python/ycm_conf_for_linux_driver.py \
     # Add more items ahead of this line if needed. \
